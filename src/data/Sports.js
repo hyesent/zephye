29b95380841e74f93db917f77969989419ca1974
hyesent <hyacinthmichael36@gmail.com>
2026-06-25 00:20:14 +0100

    Update Sports.js

--- src/data/Sports.js
+++ src/data/Sports.js
@@ -6,7 +6,7 @@ import {
   getComfortScore,
   mapWeatherCode,
   random
-} from '../utils/calculations';
+} from './calculations';
 
 export const sampleQuestions = [
   "Is it safe to play football today?",
